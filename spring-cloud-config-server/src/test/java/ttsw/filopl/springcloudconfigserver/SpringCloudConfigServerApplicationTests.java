cbd3d10005baad9f83f99d1887294b259f8f76c5
FiloPL <tzegarlicki@gmail.com>
2022-06-22 23:06:13 +0200

    v2

---
+++ spring-cloud-config-server/src/test/java/ttsw/filopl/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ttsw.filopl.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
