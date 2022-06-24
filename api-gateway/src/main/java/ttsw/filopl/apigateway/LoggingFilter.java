5a21bd984271818d22c9c5d7b240ed449907632e
FiloPL <tzegarlicki@gmail.com>
2022-06-24 12:35:13 +0200

    add 2.7.0 to v2

---
+++ api-gateway/src/main/java/ttsw/filopl/apigateway/LoggingFilter.java
@@ -0,0 +1,28 @@
+package ttsw.filopl.apigateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+/**
+ * Created by T. Filo Zegarlicki on 24.06.2022
+ **/
+
+@Component
+public class LoggingFilter implements GlobalFilter {
+
+    private Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange,
+                             GatewayFilterChain chain) {
+        logger.info("Path of the request received -> {}",
+                exchange.getRequest().getPath());
+        return chain.filter(exchange);
+    }
+}
